67e86e908ab1edb4eaaaf07a2f2a63bc5c5022f6
lalita2407 <lalitalallu02@gmail.com>
2024-08-13 15:18:26 +0530

    latest

--- variables.tf
+++ variables.tf
@@ -44,32 +44,14 @@ variable "keyvault_name" {
 }
 
 # PE
-# private endpoint
-variable "pename" {
-  type        = string
-  description = "Specifies the Name of the Private Endpoint. Changing this forces a new resource to be created."
 
-}
-# variable "location" {
-#   type        = string
-#   description = "The supported Azure location where the resource exists."
 
-# }
-# variable "resource_group_name" {
-#   type        = string
-#   description = "Specifies the Name of the Resource Group within which the Private Endpoint should exist."
-
-# }
 variable "private_endpoint_subnet_id" {
   type        = string
   description = "The ID of the Subnet from which Private IP Addresses will be allocated for this Private Endpoint."
 
 }
-# variable "private_connection_resource_id" {
-#   type        = string
-#   description = "The ID of the Private Link Enabled Remote Resource which this Private Endpoint should be connected to."
 
-# }
 variable "is_manual_connection" {
   type        = bool
   description = "Does the Private Endpoint require Manual Approval from the remote resource owner?"
@@ -86,8 +68,4 @@ variable "private_dns_zone_ids" {
   description = "Specifies the list of Private DNS Zones to include within the private_dns_zone_group."
 }
 
-variable "ip_configuration" {
-  type        = bool
-  default     = false
-  description = "One or more ip_configuration blocks as defined below. This allows a static IP address to be set for this Private Endpoint, otherwise an address is dynamically allocated from the Subnet."
-}
+
