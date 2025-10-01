deeaa9ae06ad14cbc5db1261afe4fe6f5e0079da
lalithaa2407 <lalitalallu02@gmail.com>
2025-10-01 11:47:40 +0530

    updated module provider version >4.0.0

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+
 # Creates a  MsSQL Server
 resource "azurerm_mssql_server" "example" {
   name                         = var.name
@@ -14,15 +15,6 @@ resource "azurerm_mssql_server" "example" {
     ]
   }
 }
-
-
-# # Network setting allows All Azure Services
-# resource "azurerm_mssql_firewall_rule" "example" {
-#   name             = "${var.name}-fwrule"
-#   server_id        = azurerm_mssql_server.example.id
-#   start_ip_address = "0.0.0.0"
-#   end_ip_address   = "0.0.0.0"
-# }
 
 
 # get key vault details to store DB password as secret
@@ -77,6 +69,24 @@ resource "azurerm_private_endpoint" "endpoint" {
       tags,
     ]
   }
+  depends_on = [ azurerm_mssql_server.example ]
 
 }
+# # Creates a mssql database
+# resource "azurerm_mssql_database" "test" {
+#   name                           = var.dbname
+#   server_id                      = azurerm_mssql_server.example.id
+#   create_mode                    = var.create_mode
+#   geo_backup_enabled             = var.geo_backup_enabled
+#   maintenance_configuration_name = var.maintenance_configuration_name
+#   max_size_gb                    = var.max_size_gb
+#   sku_name                       = var.sku_name
+#   storage_account_type           = var.storage_account_type
+#   lifecycle {
+#     ignore_changes = [
+#       tags,
+#     ]
+#   }
 
+#   depends_on = [ azurerm_mssql_server.example ]
+# }
--- variables.tf
+++ variables.tf
@@ -31,12 +31,6 @@ variable "connection_policy" {
   default     = "Default"
 
 }
-variable "public_network_access_enabled" {
-    type = bool
-    description = "Whether public network access is allowed for this server. Defaults to true."
-    default = false
-
-}
 
 variable "keyvault_name" {
   type = string
@@ -44,7 +38,7 @@ variable "keyvault_name" {
 }
 
 # PE
-
+# private endpoint
 
 variable "private_endpoint_subnet_id" {
   type        = string
@@ -68,4 +62,55 @@ variable "private_dns_zone_ids" {
   description = "Specifies the list of Private DNS Zones to include within the private_dns_zone_group."
 }
 
+variable "ip_configuration" {
+  type        = bool
+  default     = false
+  description = "One or more ip_configuration blocks as defined below. This allows a static IP address to be set for this Private Endpoint, otherwise an address is dynamically allocated from the Subnet."
+}
+# variable "create_mode" {
+#   type        = string
+#   description = "value"
+#   default     = "Default"
 
+
+# }
+# variable "geo_backup_enabled" {
+#   type        = bool
+#   description = "value"
+#   default     = true
+
+# }
+# variable "maintenance_configuration_name" {
+#   type        = string
+#   description = "value"
+#   default     = "SQL_Default"
+
+# }
+# variable "max_size_gb" {
+#   type        = number
+#   description = "value"
+
+# }
+# variable "sku_name" {
+#   type        = string
+#   description = "value"
+
+# }
+# variable "storage_account_type" {
+#   type        = string
+#   description = "value"
+#   default     = "Geo"
+
+# }
+# # database
+# variable "dbname" {
+#     type = string
+#     description = "value"
+
+# }
+variable "public_network_access_enabled" {
+    type = bool
+    description = "value"
+    default = false
+
+}
